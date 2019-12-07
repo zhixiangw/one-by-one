4cc98dd31262f9276f241684ebc74523b2e39c57
zhixiang_w <zhixiang_w@sina.cn>
2019-12-07 20:27:34 +0800

    订单详情以及订单列表的接口

--- pages/subPages/buy-ticket/buy-ticket.js
+++ pages/subPages/buy-ticket/buy-ticket.js
@@ -1,15 +1,13 @@
+const Api = require('../../../utils/request.js')
 Page({
   data:{
     order: {},
     pkg: {},
   },
   onLoad(params){
-    this.setData({ 
-      order: {
-        totalMoney: 0.01
-      },
+    this.setData({
       pkg: JSON.parse(decodeURIComponent(params.pkg)) || {}
-    })
+    }, this.getOrderDetail)
   },
   //模拟支付
   payment(){
@@ -23,12 +21,44 @@ Page({
       paySign,
       success: (res) => {
         wx.redirectTo({
-          url: '/pages/subPages/movie-order/movie-order',
+          url: `/pages/subPages/movie-order/movie-order`,
         })
       },
       fail: (res) => {
         console.log('fail', res)
       },
     })
+  },
+  getOrderDetail() {
+    const { pkg: { order_id } } = this.data
+    const _this = this;
+    Api.request({
+      url: `/orderDetail?order_id=${order_id}`,
+      success(res) {
+        const { cinema, movie, order } = res.data
+        _this.setData({
+          order: {
+            movieImg: movie.img.replace('w.h', '108.150'),
+            movieName: movie.name,
+            time: '2019-12-08 11:00', // 电影开播时间
+            lang: `${movie.ori_lang}${movie.ver}`,
+            cinemaName: cinema.name,
+            hall: cinema.hall, // 几号厅
+            price: order.price || 6, // 电影单价
+            salePrice: order.salePrice || 2, // 电影单价优惠售卖价
+            seat: _this.getSeatsName(order.seats),
+            totalMoney: order.amount
+          }
+        })
+      }
+    })
+  },
+  getSeatsName(seats) {
+    let seatArr = []
+    for (let i = 0; i < o.seats.length; i++) {
+      const name = o.seats[i]
+      seatArr.push(name)
+    }
+    return seatArr.join(',')
   }
 })
--- pages/subPages/movie-order/movie-order.js
+++ pages/subPages/movie-order/movie-order.js
@@ -1,3 +1,4 @@
+const Api = require('../../../utils/request.js')
 Page({
   data:{ 
     orderList:[]
@@ -8,26 +9,40 @@ Page({
     this.initData()
   },
   initData(){
-    const orderList = []
-    this.setData({
-      orderList
-    })
-  },
-  //删除订单
-  deleteOrder(e){
-    const index = e.currentTarget.dataset.index;
-    let orderList = this.data.orderList.slice();
-    orderList.splice(index,1)
-    wx.showModal({
-      title: '提示',
-      content: '确认删除订单吗？',
-      success:(res)=>{
-        if(res.confirm){
-          this.setData({
-            orderList
-          })
-          wx.setStorageSync('movieOrder', orderList)
+    const _this = this;
+    Api.request({
+      url: `/orderList?type=[1,2,4]`,
+      success(res) {
+        console.log(res)
+        const { items } = res.data
+        const statusMap = {
+          1: '已完成',
+          2: '待支付',
+          3: '待支付',
+          4: '已退款',
         }
+        _this.setData({
+          orderList: items.map(o => {
+            console.log(o)
+            let seatArr = []
+            for (let i = 0; i < o.seats.length; i++) {
+              const name = o.seats[i]
+              seatArr.push(name)
+            }
+            return {
+              cinemaName: o.ciname.name,
+              cinemaId: o.ciname.cinema_id,
+              movieImg: o.movie.img.replace('w.h', '108.150'),
+              movieName: o.movie.name,
+              seatCount: o.seats.length,
+              time: o.date, // 电影放映时间
+              hall: o.ciname.hall, // 几号厅
+              seat: seatArr.join(','),
+              totalMoney: o.amount,
+              statusName: statusMap[o.status]
+            }
+          })
+        })
       }
     })
   },
