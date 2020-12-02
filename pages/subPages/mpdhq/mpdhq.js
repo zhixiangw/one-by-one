27947fefc159c6e0df6990a3a755717307dbdc59
zhixiang_w <zhixiang_w@sina.cn>
2020-12-02 09:55:55 +0800

    添加观影通 兑换券等banner的页面元素

---
+++ pages/subPages/mpdhq/mpdhq.js
@@ -0,0 +1,76 @@
+// pages/subPages/mpdhq/mpdhq.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    couponList: [{
+      title: '爆米花',
+      subTitle: '万达电影院兑换券',
+      content: ['1、本券可免费兑换一份爆米花', '2、本券不适用其他门店', '3、本券长期效期'],
+      validDate: '2020-12-12'
+    }, {
+      title: '爆米花',
+      subTitle: '万达电影院兑换券',
+      content: ['1、本券可免费兑换一份爆米花', '2、本券不适用其他门店', '3、本券长期效期'],
+      validDate: '2020-12-12'
+    }]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
